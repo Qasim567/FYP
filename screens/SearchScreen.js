f86a283a907955892bf73263e34f351da2190a59
Qasim Farooq <qasimfarooq631@gmail.com>
2024-01-11 18:52:12 +0500

    commit

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,10 +1,17 @@
-import React from "react";
-import {View,Text,StyleSheet} from 'react-native'
+import React, { useState } from 'react';
+import { Button, View, StyleSheet } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
 
 function SearchScreen(){
+  const [result, setResult] = useState(null);
+
+  const handlePressButtonAsync = async () => {
+    let result = await WebBrowser.openBrowserAsync('http://192.168.0.101:5000/webcam');
+    setResult(result);
+  };
   return(
     <View style={style.container}>
-        <Text>Prediction Screen</Text>
+      <Button title="Open WebBrowser" onPress={handlePressButtonAsync} />
     </View>
   );
 }
