b55db691055fc881d4833c1b29712dd1aad40ab5
Qasim Farooq <qasimfarooq631@gmail.com>
2024-01-11 13:53:34 +0500

    commit

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,21 +1,25 @@
-import React from 'react';
+// SearchScreen.js
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-function SearchScreen(){
+const SearchScreen = () => {
+
+  
   return (
-        <View style={styles.container}>
-            <Text>Prediction Screen</Text>
-        </View>
+    <View style={styles.container}>
+     <Text>Prediction Screen</Text>
+   
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
-}
-
+  }
 });
 
 export default SearchScreen;
